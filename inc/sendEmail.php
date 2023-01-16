2cfbf0fc72af18877d67fe575a049bc5d3fd6777
Adam Ribeiro <aribeiro13@outlook.com>
2023-01-16 09:54:03 -0500

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,20 +1,30 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'aribeiro13@outlook.com';
 
 
 if($_POST) {
 
-   $name = trim(stripslashes($_POST['contactName']));
+   $firstName = trim(stripslashes($_POST['contactFirstName']));
+   $lastName = trim(stripslashes($_POST['contactLasttName']));
+   $phone = trim(stripslashes($_POST['contactPhone']));
    $email = trim(stripslashes($_POST['contactEmail']));
    $subject = trim(stripslashes($_POST['contactSubject']));
    $contact_message = trim(stripslashes($_POST['contactMessage']));
 
-   // Check Name
-	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+   	// Check First Name
+	if (strlen($firstName) < 2) {
+		$error['firstName'] = "Please enter your first name.";
 	}
+  	// Check Last Name
+	if (strlen($lastName) < 2) {
+		$error['lasttName'] = "Please enter your last name.";
+	}
+	// Check Phone
+	if (!preg_match('/^[+]?[1-9][0-9]{9,14}$/', $phone)) {
+		$error['phone'] = "Please enter a valid phone number.";
+
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
 		$error['email'] = "Please enter a valid email address.";
@@ -23,7 +33,7 @@ if($_POST) {
 	if (strlen($contact_message) < 15) {
 		$error['message'] = "Please enter your message. It should have at least 15 characters.";
 	}
-   // Subject
+  	 // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
 
 
